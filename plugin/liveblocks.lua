881f33b803bea11f93a7dedefe6e2f60843052f9
Justin Donato <justin@justindonato.com>
2026-01-27 07:57:10 -0500

    remove print debugging

--- plugin/liveblocks.lua
+++ plugin/liveblocks.lua
@@ -11,7 +11,6 @@ vim.api.nvim_create_autocmd({'FocusGained', 'WinEnter', 'BufEnter'}, {
   group = augroup,
   pattern = '*.md',
   callback = function()
-    print("refreshing liveblocks")
     liveblocks.refresh_liveblocks()
   end,
 })
